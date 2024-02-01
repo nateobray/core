69882b5c8b55e0ce7d579b83a343d7ec7e980995
Nate Obray <nathanobray@gmail.com>
2024-02-01 06:21:44 +0000

    trying persistant connections

--- src/data/DBConn.php
+++ src/data/DBConn.php
@@ -99,7 +99,7 @@ Class DBConn
                     $this->password,
                     array(
                         \PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
-                        \PDO::ATTR_PERSISTENT => false
+                        \PDO::ATTR_PERSISTENT => true
                     ));
                 $this->conn->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
                 $this->is_connected = true;
